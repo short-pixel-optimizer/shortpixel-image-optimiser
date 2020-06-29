b5d388e3064b818389fcf6f5096c3c77fb4391e1
Razvan Farte <farterazvan@gmail.com>
2020-06-30 00:09:45 +0300

    Empty functional test

---
+++ tests/functional/EmptyFunctionalTestCest.php
@@ -0,0 +1,13 @@
+<?php 
+
+class EmptyFunctionalTestCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+    }
+}
