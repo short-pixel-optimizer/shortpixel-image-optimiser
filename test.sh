c076ea4d75f93c11aa7cb1941f0f5793eb8a1075
Razvan Farte <farterazvan@gmail.com>
2020-06-30 16:24:30 +0300

    Refactor test script

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+vendor/bin/codecept run acceptance
+vendor/bin/codecept run functional
+vendor/bin/codecept run unit
+#  - vendor/bin/codecept run wpunit TODO Needs fixing. "Test" db install, "PHP Warning:  Cannot modify header information - headers already sent by"
 ./vendor/bin/phpunit --verbose  
