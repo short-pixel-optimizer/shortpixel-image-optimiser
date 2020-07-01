1ab9f856eb486c05ecb3c52a199b72ec817466ea
Razvan Farte <farterazvan@gmail.com>
2020-07-01 16:33:56 +0300

    Test check API key

--- tests/functional/EmptyFunctionalTestCest.php
+++ tests/functional/EmptyFunctionalTestCest.php
@@ -2,12 +2,5 @@
 
 class EmptyFunctionalTestCest
 {
-    public function _before(FunctionalTester $I)
-    {
-    }
 
-    // tests
-    public function tryToTest(FunctionalTester $I)
-    {
-    }
 }
--- tests/functional/InstallSPIOCest.php
+++ tests/functional/InstallSPIOCest.php
@@ -3,18 +3,53 @@
 
 class InstallSPIOCest
 {
-
+/*
     public function pluginNotInstalled(FunctionalTester $I)
     {
         $I->loginAsAdmin();
         $I->amOnPluginsPage();
-        $I->cantSeePluginInstalled("shortpixel-image-optimiser");
+//        $I->cantSeePluginInstalled("shortpixel-image-optimiser");
     }
 
-    public function searchPlugin(FunctionalTester $I)
+    //TODO Move to a different test suite. Normally the tests would happen on the latest commit in the github updates. this should
+    public function assumeSearchShortPixelByNameIsFirst(FunctionalTester $I)
     {
-
+        $I->loginAsAdmin();
+        $I->amOnPage("/wp-admin/plugin-install.php");
+        $I->fillField("input.plugin-search-input", "short pixel");
     }
 
+    public function assumeISeeInstallationButton(FunctionalTester $I, $scenario) {
+        $scenario->skip("Work in progress");
+    }
+
+    public function assumeISeeActivateButton(FunctionalTester $I, $scenario) {
+        //TODO After installed, the button should be deactivated
+        //This seems like a test on wordpress, so maybe drop it
+        $scenario->skip("Work in progress");
+    }
+
+    public function assumeISeeMoreDetailsButton(FunctionalTester $I) {
+        //TODO. text printed in the description is expected one
+    }
+
+    public function assumeInstallPrintsOk(FunctionalTester $I, $scenario)
+    {
+        $I->loginAsAdmin();
+        $I->amOnPage("/wp-admin/plugin-install.php");
+        $I->fillField("input.plugin-search-input", "short pixel");
+        $I->click("[data-slug=shortpixel-image-optimiser]");
+        $scenario->skip("Work in progress");
+        $I->see("Activate", "[data-slug=shortpixel-image-optimiser]");
+    }
+*/
+
+    public function activateSPIO(FunctionalTester $I) {
+        $I->loginAsAdmin();
+        $I->amOnPluginsPage();
+        $I->click("Activate");
+        $I->amOnAdminPage("options-general.php?page=wp-shortpixel-settings");
+        $I->canSeeInField("#key", "PfcGjgmofkDpSuodzYHJ"); //TODO remove api key from here
+    }
 
 }
