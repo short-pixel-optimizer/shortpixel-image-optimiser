10e4ddccb49dda9fbc455b3ecd60a231693d5d76
Razvan Farte <farterazvan@gmail.com>
2020-08-28 10:07:06 +0300

    I see attachment in FileSystem

--- tests/acceptance/MediaCept.php
+++ tests/acceptance/MediaCept.php
@@ -5,6 +5,9 @@ class MediaCept
 {
     public function uploadNewMedia(AcceptanceTester $I)
     {
+        $year  = date('Y');
+        $month = date('m');
+
         $I->loginAsAdmin();
 
         $I->amOnPage('/wp-admin/media-new.php');
@@ -17,6 +20,27 @@ class MediaCept
         $I->click('.edit-attachment');
 
         $I->seeUploadedFileFound('team.jpg','today');
+
+        $postTable = $I->grabPrefixedTableNameFor('postmeta');
+        $id = $I->grabAllFromDatabase($postTable, 'post_id', ['meta_value', '{$year}/{$month}/team.jpg']);
+
+        $I->seePostMetaInDatabase(['post_id' => $id, 'meta_key' => '_wp_attached_file', 'meta_value' => "{$year}/{$month}/team.jpg"]);
+    }
+
+    public function uploadNewMediaWrongNameShouldFail(AcceptanceTester $I)
+    {
+        $I->loginAsAdmin();
+
+        $I->amOnPage('/wp-admin/media-new.php');
+        $I->waitForText('Upload New Media');
+
+        $I->attachFile('input[type="file"]', 'team.jpg');
+
+        $I->waitForElement('.edit-attachment', 20);
+        $I->seeElement('.edit-attachment');
+        $I->click('.edit-attachment');
+
+        $I->seeUploadedFileFound('team-wrong.jpg','today');
     }
 
 }
