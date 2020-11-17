c872e0bd3ae37798202423b358b47cb813b116ca
Farte Razvan <farterazvan@gmail.com>
2020-11-17 09:42:03 +0100

    Razvanf/image optimization selenium testing (#78)
    * Merge branch 'updates' of C:\Users\Razvan\PhpstormProjects\shortpixel-image-optimiser with conflicts.
    
    * Configuration functional testing for image upload
    
    * Optimize new uploaded image (unfinished)
    
    * A new attempt for automation on media library
    
    * A new attempt for automation on media library
    
    * A new attempt for automation on media library
    
    * Attempt an acceptance test
    
    * I see attachment in FileSystem
    
    * I see attachment in FileSystem
    
    * I see attachment in FileSystem
    
    * I see attachment in FileSystem
    
    * I see attachment in FileSystem
    
    * I see attachment in FileSystem
    
    * Grab id from HTML, compare to database, try fail an acceptance test
    
    * Grab id from HTML, compare to database, try fail an acceptance test
    
    * Grab id from HTML, compare to database. Reconfigure acceptance tests browser
    
    * Wrong path fixed
    
    * Missing dump.sql and fixing caches
    
    * Missing selenium
    
    * Running in the background
    
    * Running in the background
    
    * Running in the background
    
    * Bulk test
    
    * Remove XDebug usage and warning
    
    * Bulk optimization attempt
    
    * Installing driver dependency
    
    * Disable GUI Chrome, troubleshoot chromedriver provider error
    
    * Troubleshoot chromedriver provider error
    
    * Troubleshoot chromedriver provider error
    
    * Reactivate GUI, remove redundant information
    
    * Using before script instead of throwing logs in dev/null
    
    * Installing GUI in memory buffer XVFB for headless machine
    
    * Reactivating GUIless Codeception settings
    
    * Fix before_script steps order
    
    * GUIfull and sleep after selenium startup
    
    * Wrong sleep command
    
    * Deploy test failure outputs
    
    * Fix permission denied on upload
    
    * Wrong steps order. Added sudo just in case. Fix permission denied on upload
    
    * Restrict permissions to uploads folder
    
    * Mispelled wp-content
    
    * Fix media upload test
    
    * Reverting to previous green build
    
    * Tests for API Key settings
    
    * Fix missing folder, fix 2 tests
    
    * Fix API Key tests
    
    * Install steps
    
    * Revert plugin activation suite until fixing selenium output mismatch on plugin deletion
    
    * A new attempt for automation on media library
    
    * A new attempt for automation on media library
    
    * I see attachment in FileSystem
    
    * I see attachment in FileSystem
    
    * I see attachment in FileSystem
    
    * I see attachment in FileSystem
    
    * Grab id from HTML, compare to database, try fail an acceptance test
    
    * Grab id from HTML, compare to database, try fail an acceptance test
    
    * Grab id from HTML, compare to database. Reconfigure acceptance tests browser
    
    * Wrong path fixed
    
    * Disable GUI Chrome, troubleshoot chromedriver provider error
    
    * Reactivate GUI, remove redundant information
    
    * Reactivating GUIless Codeception settings
    
    * GUIfull and sleep after selenium startup
    
    * Wrong sleep command
    
    * Deploy test failure outputs
    
    * Fix permission denied on upload
    
    * Wrong steps order. Added sudo just in case. Fix permission denied on upload
    
    * Restrict permissions to uploads folder
    
    * Install steps
    
    * Revert plugin activation suite until fixing selenium output mismatch on plugin deletion
    
    * Fixing settings page test glossy button action click
    
    * Fix. Accept Popup after changing to glossy
    
    * Fix. Firewall access for after failure archive deployment
    
    * Commit plugin activation suite for inspection
    
    * Fix. Insecure certificate curl firewall call
    
    * Fix Curl call. Extra Enclosing quotes breaks not interpreted
    
    * IPs whitelisted
    
    * Printing IP address
    
    * Change deployment server
    
    * Change deployment server
    
    * File permissions
    
    * File permissions
    
    * File permissions - just to be sure in the future
    
    * Convert CRLF to LF
    
    * Fix Failing functional tests. Re-enable unit tests
    
    * Checkout changes from updates branch for files:
    logger
    filemodel
    tools.php
    wp-offload-media.php
    img-to-picture-webp.php
    part-advanced.php
    short-pixel.css
    test-filesystem.php

---
+++ tests/acceptance/APIKeyCest.php
@@ -0,0 +1,69 @@
+<?php 
+
+class APIKeyCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function removeAPIKey(AcceptanceTester $I)
+    {
+        $I->wantToTest("How an error message is printed when removing API key from the input field");
+        $I->loginAsAdmin();
+        $I->amOnAdminPage('/options-general.php?page=wp-shortpixel-settings&part=settings');
+
+        $I->fillField(['name' => 'key'], '');
+        $I->wait(1);
+        $I->click('Save Changes');
+
+        $I->see('', ['name' => 'key']);
+        $I->see('Your API Key has been removed');
+        $I->see('In order to start the optimization process, you need to validate your API Key in the ShortPixel Settings page in your WordPress Admin.');
+
+        $I->see('Join ShortPixel', 'a.tab-link');
+        $I->dontSee('Advanced', 'a.tab-link');
+        $I->dontSee('Statistics', 'a.tab-link');
+        $I->dontSee('Debug', 'a.tab-link');
+    }
+
+    public function fillWrongAPIKeyWithoutRemovingCorrectOneFromBefore(AcceptanceTester $I) {
+        $I->wantToTest('That inserting a wrong API key over the valid, existing one, will not make the change and print a message');
+        $I->loginAsAdmin();
+        $I->amOnAdminPage('/options-general.php?page=wp-shortpixel-settings&part=settings');
+
+        $I->fillField(['name' => 'key'], 'wrongAPIKeySPIOdzYHj');
+        $I->click('Save Changes');
+
+        $I->see('Error during verifying API key: Wrong API Key.');
+//        $I->see('PfcGjgmofkDpSuodzYHJ', ['name' => 'key']);
+        $I->dontSee('Great, your API Key is valid. Please take a few moments to review the plugin settings before starting to optimize your images.');
+        $I->dontSee('wrongAPIKeySPIOdzYHj');
+        $I->see('Your API key is valid.');
+
+        $I->wantTo('See if settings page still displays the advanced settings and so on');
+        $I->see('Advanced', 'a.tab-link');
+        $I->see('Statistics', 'a.tab-link');
+        $I->dontSee('Debug', 'a.tab-link');
+    }
+
+    public function fillWrongAPIKeyAfterRemovingCorrectOneFromBefore(AcceptanceTester $I) {
+        $I->wantToTest('That an error message is displayed and no other previous API key is filled in after error');
+
+        $this->removeAPIKey($I);
+        $I->see('', '#key');
+
+        $I->fillField('#key', 'wrongAPIKeySPIOdzYHj');
+        $I->click('Validate');
+
+        $I->see('Error during verifying API key: Wrong API Key.');
+        $I->see('In order to start the optimization process, you need to validate your API Key in the ShortPixel Settings page in your WordPress Admin.');
+        $I->dontSee('Your API key is valid.');
+        $I->dontSee('Great, your API Key is valid. Please take a few moments to review the plugin settings before starting to optimize your images.');
+        $I->dontSee('wrongAPIKeySPIOdzYHj');
+
+        $I->see('Join ShortPixel', 'a.tab-link');
+        $I->dontSee('Advanced', 'a.tab-link');
+        $I->dontSee('Statistics', 'a.tab-link');
+        $I->dontSee('Debug', 'a.tab-link');
+    }
+}
