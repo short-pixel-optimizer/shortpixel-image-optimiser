7d32b36a69b726631a23aed3cbe32c99d686f234
Pedro Dobrescu <pedro@presslabs.com>
2022-01-14 16:33:29 -0300

    Fix upgrade pop-up styles

--- class/view/snippets/part-upgrade-options.php
+++ class/view/snippets/part-upgrade-options.php
@@ -6,7 +6,7 @@ namespace ShortPixel;
 
   <div id="shortPixelProposeUpgradeShade" class="sp-modal-shade" style="display:none;"></div>
 
-	<div id="shortPixelProposeUpgrade" class="shortpixel-modal shortpixel-hide" style="min-width:610px;margin-left:-305px;">
+	<div id="shortPixelProposeUpgrade" class="shortpixel-modal shortpixel-hide" style="min-width:650px;margin-left:-305px;">
       <div class="sp-modal-title">
           <button type="button" class="sp-close-upgrade-button" onclick="ShortPixel.closeProposeUpgrade()">&times;</button>
            <?php __('Upgrade your ShortPixel account', 'shortpixel-image-optimiser')  ?>
