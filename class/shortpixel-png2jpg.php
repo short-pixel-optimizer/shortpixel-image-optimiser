70de2f73816ceceb9d6761fdb936ff4917554db1
Bas Schuiling <bas@weblogmechanic.com>
2022-10-04 14:04:12 +0300

    Fix - WPMLDuplicates added to png2jpg method

--- class/shortpixel-png2jpg.php
+++ class/shortpixel-png2jpg.php
@@ -406,6 +406,8 @@ class ShortPixelPng2Jpg {
         $newFile = $params['file'];
         $attach_id = $imageObj->get('id');
 
+				$WPMLduplicates = $imageObj->getWPMLDuplicates();
+
 				// This action prevents images from being regenerated on the thumbnail hook.
 			  	do_action('shortpixel-thumbnails-before-regenerate', $attach_id );
 
@@ -446,6 +448,16 @@ class ShortPixelPng2Jpg {
         Log::addDebug('Png2Jpg New Metadata' . $attach_id, $new_metadata);
 		//		wp_update_post(array('ID' => $attach_id, 'post_mime_type' => 'image/jpeg' ));
         $bool = wp_update_attachment_metadata($attach_id, $new_metadata);
+
+
+				if (is_array($WPMLduplicates) && count($WPMLduplicates) > 0)
+				{
+					 foreach ($WPMLduplicates as $duplicate_id)
+					 {
+						  update_attached_file($duplicate_id, $newFile->getFullPath() );
+							wp_update_attachment_metadata($duplicate_id, $new_metadata);
+					 }
+				}
 
         return $new_metadata;
 
