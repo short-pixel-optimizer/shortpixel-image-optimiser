d0beb6e16491cb9644835d5cd18664b012f9dc65
bschuiling <bas@weblogmechanic.com>
2022-11-23 12:54:18 -0500

    Update UiHelper.php
    Fix for PHP 5.6, who doesn't like spaceships, nor their operators..

--- class/Helper/UiHelper.php
+++ class/Helper/UiHelper.php
@@ -104,9 +104,13 @@ class UiHelper
          $output .= '<div class="totals">' . sprintf(__('+%s thumbnails optimized','shortpixel-image-optimiser'), self::formatNumber($thumbsDone, 0)) . '</div>';
 
 			 $improvs = array();
-
+	    
 			 uasort($improvements['thumbnails'], function ($a, $b) {
-				 	return $b[0] <=> $a[0];
+					//return $b[0] <=> $a[0]; // @todo Efficient code to use once PHP 5 support is done.
+					if ($a == $b) {
+						return 0;
+					}
+					return ($b < $a) ? -1 : 1;
 			 });
 
 			 $cutoff = false;
