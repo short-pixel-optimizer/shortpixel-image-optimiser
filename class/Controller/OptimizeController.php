d3ace6a57f1673ccd2f8ae38516cea16ae519e2f
Bas Schuiling <bas@weblogmechanic.com>
2023-04-15 10:28:00 -0600

    Removing test crash

--- class/Controller/OptimizeController.php
+++ class/Controller/OptimizeController.php
@@ -447,7 +447,6 @@ class OptimizeController
 
 			}
 
-hark();
 			// If item is blocked (handling success), skip over. This can happen if internet is slow or process too fast.
 			if (property_exists($item, 'blocked') && true === $item->blocked )
 			{
