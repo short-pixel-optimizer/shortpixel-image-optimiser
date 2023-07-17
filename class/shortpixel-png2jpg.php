3bd372e10903951ecb7efdfeb11ac7699d98ef71
Bas Schuiling <bas@weblogmechanic.com>
2023-07-17 19:51:07 +0300

    Namespace fix?

--- class/shortpixel-png2jpg.php
+++ class/shortpixel-png2jpg.php
@@ -1,10 +1,10 @@
 <?php
- namespace ShortPixel;
+namespace ShortPixel;
 
  if ( ! defined( 'ABSPATH' ) ) {
   exit; // Exit if accessed directly.
  }
- 
+
  use ShortPixel\ShortPixelLogger\ShortPixelLogger as Log;
  use ShortPixel\Model\Image\ImageModel as ImageModel;
  use ShortPixel\Model\Image\MediaLibraryModel as MediaLibraryModel;
