315d5a7a095f10dd47ad48e8b860b7489f107859
Bas Schuiling <bas@weblogmechanic.com>
2024-05-08 14:10:34 +0300

    Major improvement to formidable integration

--- class/external/formidable.php
+++ class/external/formidable.php
@@ -12,11 +12,16 @@ class Formidable
 {
     public function __construct()
     {
-        $this->addHooks();
+        if (true === \wpSPIO()->env()->is_autoprocess )
+        {
+          $this->addHooks();
+        }
+
     }
 
     protected function addHooks()
     {
+
         add_filter('shortpixel/media/uploadhook', array($this, 'checkFormUpload'), 10, 4);
         add_action('frm_after_update_entry', array($this, 'formUpload'), 10, 2);
         add_action('frm_after_create_entry', array($this, 'formUpload'), 30, 2);
@@ -30,6 +35,7 @@ class Formidable
         // Seems form submit temporary.
         if (is_numeric($value))
         {
+          Log::addTemp('Numeric post meta on formidable, preventing');
            return false;
         }
 
@@ -38,29 +44,87 @@ class Formidable
 
     public function formUpload($id, $new_values)
     {
-       $fs = \wpSPIO()->filesystem();
+       $form_id = isset($_POST['form_id']) ? intval($_POST['form_id']) : null;
 
-       if (isset($_POST['item_meta']) && is_array($_POST['item_meta']))
+       if (is_null($form_id))
        {
-           foreach($_POST['item_meta'] as $field_id => $meta_id)
-           {
-             // array can contain non numeric or empty values.
-             if (! is_numeric($meta_id))
-             {
-              continue;
-             }
+          Log::addError('Form ID not set, aborting', $_POST);
+          return;
+       }
 
-              $mediaItem = $fs->getMediaImage($meta_id);
-              if (is_object($mediaItem) && $mediaItem->isProcessable())
-              {
-                 $adminController = AdminController::getInstance();
-                 $adminController->handleImageUploadHook(null, $meta_id);
-              }
-           }
+       if (false === isset($_POST['item_meta']) || false === is_array($_POST['item_meta']))
+       {
+          Log::addTemp('Not Post item meta here');
+          return;
+       }
+
+       $fields = $this->getFileUploadFields($form_id);
+       if (false === $fields)
+       {
+          Log::addTemp('Form fields didnt contain uploads');
+          return;
+       }
+
+        //$item_meta = array_filter($_POST['item_meta']);
+
+       foreach($fields as $index => $field_id)
+       {
+         $meta = isset($_POST['item_meta'][$field_id]) ? $_POST['item_meta'][$field_id] : '';
+
+         // array can contain non numeric or empty values.
+         if (! is_numeric($meta) && ! is_array($meta))
+         {
+          continue;
+         }
+         elseif (is_array($meta)) // can be nested.
+         {
+            $meta = array_filter($meta);
+            foreach($meta as $index => $meta_id)
+            {
+               $this->checkMediaLibrary(intval($meta_id));
+            }
+         }
+         else {
+            $this->checkMediaLibrary(intval($meta));
+         }
+
 
        }
 
     }
+
+    private function getFileUploadFields($form_id)
+    {
+        global $wpdb;
+
+        $sql = 'SELECT id FROM ' . $wpdb->prefix . 'frm_fields where form_id = %d and type = %s ';
+        $sql = $wpdb->prepare($sql, $form_id, 'file');
+
+        $row = $wpdb->get_col($sql);
+
+        if (count($row) === 0)
+        {
+           return false;
+        }
+
+        return $row;
+
+    }
+
+    private function checkMediaLibrary($item_id)
+    {
+      Log::addTemp('Checking Media Lib: ' . $item_id);
+      $fs = \wpSPIO()->filesystem();
+
+      $mediaItem = $fs->getMediaImage($item_id);
+      if (is_object($mediaItem) && $mediaItem->isProcessable())
+      {
+         Log::addTemp('Adding item - ' . $item_id);
+         $adminController = AdminController::getInstance();
+         $adminController->handleImageUploadHook(null, $item_id);
+      }
+    }
+
 
 
 }
