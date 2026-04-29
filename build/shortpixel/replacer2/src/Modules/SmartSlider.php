416c9e1027a1260f27fcf9f4df423e422c6754cb
Bas Schuiling <bas@weblogmechanic.com>
2026-04-29 18:59:40 +0300

    Smartslider integration off until fix is found for the slide / base64 encoding on images

--- build/shortpixel/replacer2/src/Modules/SmartSlider.php
+++ build/shortpixel/replacer2/src/Modules/SmartSlider.php
@@ -13,7 +13,7 @@ class SmartSlider
 
     protected $base_url;
     protected $subdir;
-    protected $basedir; 
+    protected $basedir;
 
     public static function getInstance($replacer)
     {
@@ -31,14 +31,13 @@ class SmartSlider
              return; 
         }
 
+         return false; 
+
         $this->replacer = $replacer; 
 
         $uploads_dir = \wp_upload_dir(); 
 
         $this->base_url = str_replace($uploads_dir['subdir'], '', $uploads_dir['baseurl']); 
-        /** THIS fatally doesn't work because subdir in uploads dir is *current month*, not the month of the file-upload per-se 
-         *  So would need to find a way to get this part without current upload dir. 
-         */
         $this->subdir = $uploads_dir['subdir'];
         $this->basedir = $uploads_dir['basedir']; 
 
@@ -51,9 +50,13 @@ class SmartSlider
         global $wpdb; 
         $table = $wpdb->prefix . 'nextend2_smartslider3_slides'; 
 
-        $base_url = $this->convertToFormat($base_url);
+       $base_url = $this->convertToFormat($base_url);
         $search_urls = array_map([$this, 'convertToFormat'], $search_urls); 
-        $replace_urls = array_map([$this, 'convertToFormat'], $replace_urls); 
+        $replace_urls = array_map([$this, 'convertToFormat'], $replace_urls);
+
+
+           Log::addTemp('BaseURL', $base_url); 
+        Log::addTemp('SearchU', $search_urls);
 
         $select_sql = 'SELECT * FROM %i where %i like %s OR %i like %s OR %i LIKE %s'; 
 
@@ -63,49 +66,75 @@ class SmartSlider
                 '%' . $base_url . '%', 
                 'params', 
                 '%' . $base_url . '%',
-                'slide', 
-                '%' . $base_url . '%',
+                                'slide', 
+                '%' . $base_url . '%'
             ]);              
                         
+     //       Log::addTemp('Query', $prepared_select);
             $results = $wpdb->get_results($prepared_select, ARRAY_A);
 
             foreach($results as $index => $data)
             {
 
-                $update = []; // prevent hanging updates from previous records
                 $row_id = $data['id']; 
                 $thumbnail = $this->replacer->replaceContent($data['thumbnail'], $search_urls, $replace_urls); 
                 $params = $this->replacer->replaceContent($data['params'], $search_urls, $replace_urls); 
                 $slide = $this->replacer->replaceContent($data['slide'], $search_urls, $replace_urls); 
 
+                Log::addTemp('Row found - ' . $row_id);
+                $update = []; 
                 if ($thumbnail != $data['thumbnail'])
                 {
                     $update['thumbnail'] = $thumbnail; 
                 }
                 if ($params !== $data['params'])
                 {
-                    $update['params'] = $params; 
+                    $update['params'] = $this->checkFixEncoding($params); 
                 }
                 if ($slide !== $data['slide'])
                 {
-                    $update['slide'] = $slide; 
+                    $update['slide'] = $this->checkFixEncoding($slide); 
                 }
-
+                
                 if (count($update) > 0)
                 {
                     $res = $wpdb->update($table, $update, ['id' => $row_id], '%s', '%d' );
-                    Log::addInfo('Smartslider Updated records: ' . $res);   
+                    Log::addInfo('Smartslider Updated records: ' . $res, $update);   
                 }
             }
     
     }
 
-    protected function fixUploadString($value)
+    
+    protected function checkFixEncoding($params)
     {
-        $subpos = strstr($value, $this->subdir); 
-        $value = '$upload$' . $subpos;
-      //  $value = str_replace($this->base_url, '$uploads$', $value); 
-        return $value; 
+        $decoded_params = json_decode($params, true);
+
+        if (! is_array($decoded_params)) {
+            return $params;
+        }
+
+        $params = $this->encodeFontValues($decoded_params);
+        return json_encode($params);
+    }
+
+    protected function encodeFontValues($value)
+    {
+        if (! is_array($value)) {
+            return $value;
+        }
+
+        $result = [];
+        foreach ($value as $key => $item) {
+            if ('font' === $key) {
+                Log::addTemp('Found key - updating ', $item);
+                $result[$key] = is_string($item) ? base64_encode($item) : $item;
+            } else {
+                $result[$key] = $this->encodeFontValues($item);
+            }
+        }
+
+        return $result;
     }
 
     protected function convertToFormat($relpath) {
@@ -119,6 +148,7 @@ class SmartSlider
     if ($uploadsPos !== false) {
         // Get the portion after 'uploads/'
         $filePortion = substr($relpath, $uploadsPos + strlen($uploadsDirName) + 1);
+        
         $result = '$upload$/' . $filePortion;
         return $result;
     }
