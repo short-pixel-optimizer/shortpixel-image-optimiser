9d242b493f1c24d39c8d98c3895a5056ed84840d
Bas Schuiling <bas@weblogmechanic.com>
2026-06-16 10:50:32 +0300

    Incorrect filesizes after restore / backup because of failing wpmeta value restore

--- class/Controller/Optimizer/ActionController.php
+++ class/Controller/Optimizer/ActionController.php
@@ -160,7 +160,6 @@ class ActionController extends OptimizerBase
             $qStatus->numitems = 1; 
       }
 
-      Log::addTEmp('Returning actoin: ' . $action, $qStatus);
 
       return $qStatus;
   //  }
