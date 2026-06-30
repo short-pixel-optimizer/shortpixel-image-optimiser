cf2fda4ddf108cc1636f52e96a4ba60fbc3a3962
Pedro Dobrescu <petre.dobrescu@gmail.com>
2026-06-30 21:24:17 +0300

    Add the latest version of the automated testing

--- debug.sh
+++
@@ -1,2 +0,0 @@
-#! /usr/bin/env bash
-curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token q3IYSHdwN_XsA7FcFvJxsw" -d '{ "quiet": true }' https://api.travis-ci.com/job/$1/debug
